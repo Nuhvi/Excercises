a917f4efe007462e372afe099f7da4721375163f
nazeh <ar.nazeh@gmail.com>
2019-11-21 19:55:02 +0300

    add gitignore to express test app

--- node/basic-info-expressJS/index.js
+++ node/basic-info-expressJS/index.js
@@ -1,34 +1,10 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-let Page404;
-
-fs.readFile('./404.html', (err, data) => {
-  Page404 = data;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-const fileNameFromPath = (path) => {
-  path = path.match(/\w+/);
-  path = path ? path[0] : 'index';
-  return `./${path}.html`;
-};
-
-http
-  .createServer((req, res) => {
-    const q = url.parse(req.url, true);
-    const fileName = fileNameFromPath(q.pathname);
-
-    fs.readFile(fileName, (err, data) => {
-      let code = 200;
-
-      if (err) {
-        code = 404;
-        data = Page404;
-      }
-
-      res.writeHead(code, { 'Content-Type': 'text/html' });
-      res.end(data);
-    });
-  })
-  .listen(8080);
+app.listen('8000', () => {
+  console.log('Example app listening on port http://localhost:8000/ !');
+});
